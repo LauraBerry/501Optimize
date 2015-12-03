f77321302fc63970e6707767b04cc4434f858a4a
lnberry <lnberry@compute-linux03.cs.ucalgary.ca>
2015-12-03 12:23:56 -0700

    changed base code

--- convolve.cpp
+++ convolve.cpp
@@ -1,56 +1,196 @@
-//important note everywhere it says SizeOfChunk2 is wrong Laura
-
-
-/*  Include files  */
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/time.h>
-#include <unistd.h>
-#include <iostream>
-#include <string.h>
-#include <math.h>
-#define SIZE       8
-#define PI         3.141592653589793
-#define TWO_PI     (2.0 * PI)
-#define SWAP(a,b)  tempr=(a);(a)=(b);(b)=tempr
+#include<math.h>
+#include<iostream>
 using namespace std;
 
-char * inFile;
-char * InputResponse;
-char * outFile;
-int inFileSize=0;
-int sizeOfSignal=0;
+char chunkID[5];
+int chunkSize;
+char format[5];
 
+char subChunk1ID[5];
+int subChunk1Size;
+short audioFormat;
+short numChannels;
+int sampleRate;
+int byteRate;
+short blockAlign;
+short bitsPerSample;
 
-typedef struct header_file
+int channelSize;
+char subChunk2ID[5];
+int subChunk2Size;
+
+short* data;
+
+void print()
 {
-    char chunk_id[4];
-    int chunk_size;
-    char format[4];
-    char subchunk1_id[4];
-    int subchunk1_size;
-    short int audio_format;
-    short int num_channels;		// number of channels i.e 1-mono 2-stereo
-    int sample_rate;			// sample_rate denotes the sampling rate.
-    int byte_rate;
-    short int block_align;
-    short int bits_per_sample;		// number of bits per sample
-} header;
-typedef struct header_file* header_p;
+	chunkID[5] = '\0';
+	format[5] = '\0';
+	subChunk1ID[5] = '\0';
+	subChunk2ID[5] = '\0';
+	
+	printf("\n============= HEADER INFO =============\n");
+	printf(" chunkID:%s\n", chunkID);
+	printf(" chunkSize:%d\n", chunkSize);
+	printf(" format:%s\n", format);
+	printf(" subChunk1ID:%s\n", subChunk1ID);
+	printf(" subChunk1Size:%d\n", subChunk1Size);
+	printf(" audioFormat:%d\n", audioFormat);
+	printf(" numChannels:%d\n", numChannels);
+	printf(" sampleRate:%d\n", sampleRate);
+	printf(" byteRate:%d\n", byteRate);
+	printf(" blockAlign:%d\n", blockAlign);
+	printf(" bitsPerSample:%d\n", bitsPerSample);
+	printf(" subChunk2ID:%s\n", subChunk2ID);
+	printf(" subChunk2Size:%d\n", subChunk2Size);
+}
 
-/*  Function prototypes  */
-void convolve(float x[], int N, float h[], int M, float y[], int P);
-void print_vector(char *title, float x[], int N);
+int loadWave(char* filename)
+{
+	FILE* in = fopen(filename, "rb");
 
+	if (in != NULL)
+	{		
+		printf("Reading %s...\n",filename);
 
-/*****************************************************************************
-*
-*    Function:     main
-*
-*    Description:  Tests the convolve function with various input signals
-*
-*****************************************************************************/
-/*this code is taken from code provided by Prof. Manzara*/
+		fread(chunkID, 1, 4, in);
+		fread(&chunkSize, 1, 4, in);
+		fread(format, 1, 4, in);
+
+		//sub chunk 1
+		fread(subChunk1ID, 1, 4, in);
+		fread(&subChunk1Size, 1, 4, in);
+		fread(&audioFormat, 1, 2, in);
+		fread(&numChannels, 1, 2, in);
+		fread(&sampleRate, 1, 4, in);
+		fread(&byteRate, 1, 4, in);
+		fread(&blockAlign, 1, 2, in);
+		fread(&bitsPerSample, 1, 2, in);		
+		
+		//read extra bytes
+		if(subChunk1Size == 18)
+		{
+			short empty;
+			fread(&empty, 1, 2, in);		
+		}
+		
+		//sub chunk 2
+		fread(subChunk2ID, 1, 4, in);
+		fread(&subChunk2Size, 1, 4, in);
+
+		//read data		
+		int bytesPerSample = bitsPerSample/8;
+		int numSamples = subChunk2Size / bytesPerSample;
+		data = (short*) malloc(sizeof(short) * numSamples);
+		
+		//fread(data, 1, bytesPerSample*numSamples, in);
+		
+		int i=0;
+		short sample=0;
+		while(fread(&sample, 1, bytesPerSample, in) == bytesPerSample)
+		{		
+			data[i++] = sample;
+			sample = 0;			
+		}
+		
+		fclose(in);
+		printf("Closing %s...\n",filename);
+	}
+	else
+	{
+		printf("Can't open file\n");
+		return 0;
+	}
+	return 1;
+}
+
+int saveWave(char* filename)
+{
+	FILE* out = fopen(filename, "wb");
+
+	if (out != NULL)
+	{		
+		printf("\nWriting %s...\n",filename);
+
+		fwrite(chunkID, 1, 4, out);
+		fwrite(&chunkSize, 1, 4, out);
+		fwrite(format, 1, 4, out);
+
+		//sub chunk 1
+		fwrite(subChunk1ID, 1, 4, out);
+		fwrite(&subChunk1Size, 1, 4, out);
+		fwrite(&audioFormat, 1, 2, out);
+		fwrite(&numChannels, 1, 2, out);
+		fwrite(&sampleRate, 1, 4, out);
+		fwrite(&byteRate, 1, 4, out);
+		fwrite(&blockAlign, 1, 2, out);
+		fwrite(&bitsPerSample, 1, 2, out);		
+		
+		//read extra bytes
+		if(subChunk1Size == 18)
+		{
+			short empty = 0;
+			fwrite(&empty, 1, 2, out);		
+		}
+		
+		//sub chunk 2
+		fwrite(subChunk2ID, 1, 4, out);
+		fwrite(&subChunk2Size, 1, 4, out);
+
+		//read data		
+		int bytesPerSample = bitsPerSample / 8;
+		int sampleCount =  subChunk2Size / bytesPerSample;
+		
+		//impulse response - echo
+		int IRSize = 6;
+		float IR[IRSize];
+		IR[0] = 1.0;
+		IR[1] = 1.0;
+		IR[2] = 1.0;
+		IR[3] = 1.0;
+		IR[4] = 1.0;
+		IR[5] = 1.0;
+		
+		//write the data
+		float* newData = (float*) malloc(sizeof(float) * (sampleCount + IRSize - 1));
+		float maxSample = -1;
+		float MAX_VAL = 32767.f;	//FIXME: find based on bits per sample
+			
+		for(int i=0; i<sampleCount; ++i)
+		{			
+			//convolve
+			for(int j=0; j<IRSize; ++j)
+				newData[i+j] += ((float)data[i] / MAX_VAL) * IR[j];
+			
+			//Keep track of max value for scaling
+			if(i==0)
+				maxSample = newData[0];
+			else if(newData[i] > maxSample)
+				maxSample = newData[i];
+		}		
+		
+		//scale and re write the data
+		for(int i=0; i<sampleCount + IRSize - 1; ++i)
+		{
+			newData[i] = (newData[i] / maxSample) ;
+			short sample = (short) (newData[i] * MAX_VAL);
+			fwrite(&sample, 1, bytesPerSample, out);
+		}
+		
+		//clean up
+		free(newData);
+		fclose(out);
+		printf("Closing %s...\n",filename);
+	}
+	else
+	{
+		printf("Can't open file\n");
+		return 0;
+	}
+	return 1;
+}
+
 void method(double data[], int nn, int isign)
 {
     unsigned long n, mmax, m, j, istep, i;
@@ -64,8 +204,12 @@ void method(double data[], int nn, int isign)
 	{
 		if (j > i) 
 		{
-			SWAP(data[j], data[i]);
-			SWAP(data[j+1], data[i+1]);
+			double holder=data[j];
+			data[j]=data[i];
+			data[i]=holder;
+			holder=data[j+1];
+			data[j+1]=data[i+1];
+			data[i+1]=holder;
 		}
 		m = nn;
 		while (m >= 2 && j > m) 
@@ -85,160 +229,32 @@ void method(double data[], int nn, int isign)
 	wpi = sin(theta);
 	wr = 1.0;
 	wi = 0.0;
-	for (m = 1; m < mmax; m += 2) {
-	    for (i = m; i <= n; i += istep) {
-		j = i + mmax;
-		tempr = wr * data[j] - wi * data[j+1];
-		tempi = wr * data[j+1] + wi * data[j];
-		data[j] = data[i] - tempr;
-		data[j+1] = data[i+1] - tempi;
-		data[i] += tempr;
-		data[i+1] += tempi;
+	for (m = 1; m < mmax; m += 2) 
+	{
+	    for (i = m; i <= n; i += istep)
+		{
+			j = i + mmax;
+			tempr = wr * data[j] - wi * data[j+1];
+			tempi = wr * data[j+1] + wi * data[j];
+			data[j] = data[i] - tempr;
+			data[j+1] = data[i+1] - tempi;
+			data[i] += tempr;
+			data[i+1] += tempi;
 	    }
 	    wr = (wtemp = wr) * wpr - wi * wpi + wr;
 	    wi = wi * wpr + wtemp * wpi + wi;
 	}
-	mmax = istep;
+		mmax = istep;
     }
 }
 
-
-/*****************************************************************************
-*
-*    Function:     convolve
-*
-*    Description:  Convolves two signals, producing an output signal.
-*                  The convolution is done in the time domain using the
-*                  "Input Side Algorithm" (see Smith, p. 112-115).
-*
-*    Parameters:   x[] is the signal to be convolved
-*                  N is the number of samples in the vector x[]
-*                  h[] is the impulse response, which is convolved with x[]
-*                  M is the number of samples in the vector h[]
-*                  y[] is the output signal, the result of the convolution
-*                  P is the number of samples in the vector y[].  P must
-*                       equal N + M - 1
-*
-*****************************************************************************/
-
-void convolve(float x[], int N, float h[], int M, float y[], int P)
+int main(int argc, char* argv[])
 {
-  int n, m;
-
-  /*  Make sure the output buffer is the right size: P = N + M - 1  */
-  if (P != (N + M - 1)) 
-  {
-    printf("Output signal vector is the wrong size\n");
-    printf("It is %-d, but should be %-d\n", P, (N + M - 1));
-    printf("Aborting convolution\n");
-    return;
-  }
-
-  /*  Clear the output buffer y[] to all zero values  */  
-  for (n = 0; n < P; n++)
-    y[n] = 0.0;
-
-  /*  Do the convolution  */
-  /*  Outer loop:  process each input value x[n] in turn  */
-  for (n = 0; n < N; n++) 
-  {
-    /*  Inner loop:  process x[n] with each sample of h[]  */
-    for (m = 0; m < M; m++)
-      y[n+m] += x[n] * h[m];
-  }
-}
-
-
-
-/*****************************************************************************
-*
-*    Function:     print_vector
-*
-*    Description:  Prints the vector out to the screen
-*
-*    Parameters:   title is a string naming the vector
-*                  x[] is the vector to be printed out
-*                  N is the number of samples in the vector x[]
-*
-*****************************************************************************/
-
-void print_vector(char *title, float x[], int N)
-{
-  int i;
-
-  printf("\n%s\n", title);
-  printf("Vector size:  %-d\n", N);
-  printf("Sample Number \tSample Value\n");
-  for (i = 0; i < N; i++)
-    printf("%-d\t\t%f\n", i, x[i]);
-}
-
-/*
-* function reader
-* description: reads the input file
-*/
-void reader(char * in, char * inResponse)
-{
-	cout<<"reader"<<endl;
-	FILE * input=fopen(in, "rb"); 						//reading the file in binary
+	char* filename = argv[1];
 	
-	header_p head= (header_p)malloc(sizeof(header));
+	if(loadWave(filename))
+		print();
 	
-	if(in == NULL)
-	{
-		//do something
-	}
-	else
-	{
-		fseek(input, 0, SEEK_END);
-		inFileSize=ftell(input);						//length of input file
-		rewind(input);
-		
-		char * fileContent= (char *) malloc (inFileSize);
-		fread(fileContent, inFileSize, 1, input);			//read input file
-		rewind(input);
-		
-		fread(head, 1, sizeof(head), input);			//get header info
-		int a=0;
-		for(int i=0;i<inFileSize-5;i++)
-		{
-			if(fileContent[i]=='d' && fileContent[i+1]=='a' && fileContent[i+2]=='t' && fileContent[i+3]==fileContent[i+1])
-			{
-				a=i+4;
-			}
-		}
-		fseek(input, a, SEEK_SET);
-		char * SizeOfChunk2;
-		fread(SizeOfChunk2, 4,1, input);
-		
-		fseek(input, a+4, SEEK_SET);
-		char * data= (char*)malloc(sizeof(SizeOfChunk2));
-		fread(data,sizeof(SizeOfChunk2),1,input);
-		fclose(input);
-		
-		short * smokeSignal = (short*)malloc(sizeof(SizeOfChunk2));
-		int lengthSmokeSignal=sizeof(SizeOfChunk2)/2;
-		for(int j=0; j<sizeOfSignal;j++)
-		{
-			short half= (short) ((unsigned char) sizeof(SizeOfChunk2));
-			short otherHalf=(short)((unsigned char)sizeof(SizeofChunk2));
-		}			
-	}
-}
-
-
-int main(int argc, char ** argv)
-{
-	cout<<"main"<<endl;
-	if(argc<4)
-	{
-		return 0;
-	}
-	inFile=argv[1];
-	outFile=argv[2];
-	InputResponse=argv[3];
-	
-	reader(inFile, InputResponse);
-	
-	return 0;
+	saveWave("out.wav");
+	free(data);
 }
